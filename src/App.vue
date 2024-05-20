eec1e303b620db372e67ff5bd3a8a02709c2c3f1
Tristan Jay J. Virtudazo <tj.virtudazo@gmail.com>
2024-05-20 16:15:43 +0800

    modify: importing data from task store and inject them to taskboard component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
+  import { storeToRefs } from 'pinia'
   import VTaskBoard from '@/components/VTaskBoard.vue'
+  import { useTaskStore } from '@/stores/task'
+
+  const taskStore = useTaskStore()
+
+  const { todoList, inprogList, doneList } = storeToRefs(taskStore)
 </script>
 
 <template>
   <main>
-    <VTaskBoard title="To Do"/>
-    <VTaskBoard title="In Progress"/>
-    <VTaskBoard title="Done"/>
+    <VTaskBoard :tasks="todoList" title="To Do"/>
+    <VTaskBoard :tasks="inprogList" title="In Progress"/>
+    <VTaskBoard :tasks="doneList" title="Done"/>
   </main>
 </template>
 
