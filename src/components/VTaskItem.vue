01d3e8146a0278811937d52b47e8e651e1d36a92
Tristan Jay J. Virtudazo <tj.virtudazo@gmail.com>
2024-05-20 15:21:43 +0800

    create: VTaskItem for dislaying individual task

---
+++ src/components/VTaskItem.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+	const props = defineProps<{ task?: Task }>()
+</script>
+
+<template>
+	<li>
+		<div>
+			<h3>{{ task.name }}</h3>
+			<small>{{ task.status }}</small>
+			<p>{{ task.description ? task.description : "None" }}</p>
+		</div>
+		<div class="btn__group">
+			<button>Edit</button>
+			<button>Start task</button>
+			<button>Mark as complete</button>
+		</div>
+	</li>
+</template>
+
+<style scoped>
+	.btn__group {
+		
+	}
+</style>
