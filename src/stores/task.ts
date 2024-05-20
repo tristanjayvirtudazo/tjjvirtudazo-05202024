584e747d31a905600734b55c3f49c18b3634425c
Tristan Jay J. Virtudazo <tj.virtudazo@gmail.com>
2024-05-20 15:51:01 +0800

    modify: add dummy data to tasks list

--- src/stores/task.ts
+++ src/stores/task.ts
@@ -6,31 +6,31 @@ export const useTaskStore = defineStore('task', () => {
 		{
 		    "id": "a47e88a8-1c0f-4d61-a3d6-8a5b6f0f4f2a",
 		    "name": "Task 1",
-		    "status": "Pending",
+		    "status": "TO DO",
 		    "description": "This is the first task."
 		  },
 		  {
 		    "id": "b59f1b85-6b4c-4a9a-a2d3-ecb6e8ed9f4e",
 		    "name": "Task 2",
-		    "status": "In Progress",
+		    "status": "IN PROGRESS",
 		    "description": "This task is currently being worked on."
 		  },
 		  {
 		    "id": "c68e2e99-2f45-4298-9d4c-8157d6b6f67e",
 		    "name": "Task 3",
-		    "status": "Completed",
+		    "status": "DONE",
 		    "description": "This task has been completed."
 		  },
 		  {
 		    "id": "d78f3a7d-3e5b-4e99-9b2c-2f78e6f7f4d8",
 		    "name": "Task 4",
-		    "status": "Pending",
+		    "status": "TO DO",
 		    "description": "This task is waiting to be started."
 		  },
 		  {
 		    "id": "e89f4b8a-4e0c-4a8a-9d5d-3f89f9e8f8e7",
 		    "name": "Task 5",
-		    "status": "In Progress"
+		    "status": "IN PROGRESS"
 		  }
 	])
 	const name: Ref<string> = ref('')
