92c1daac9050b47af647eadf7c1297e7792b7098
Tristan Jay J. Virtudazo <tj.virtudazo@gmail.com>
2024-05-20 15:09:54 +0800

    create: VTaskBoard component

---
+++ src/components/VTaskBoard.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+	const props = defineProps(['title'])
+</script>
+
+<template>
+	<section class="board__container">
+		<h2>{{ title }}</h2>
+
+		<ul>
+			<li></li>
+		</ul>
+	</section>
+</template>
+
+<style scoped>
+	.board__container {
+
+	}
+</style>
