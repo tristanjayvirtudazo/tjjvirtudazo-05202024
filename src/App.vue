ded7c770763b7bb5fcad0d831fc4dbb838859f4e
Tristan Jay J. Virtudazo <tj.virtudazo@gmail.com>
2024-05-20 15:57:54 +0800

    implement: VTaskBoard implementation to App file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+  import VTaskBoard from '@/components/VTaskBoard.vue'
 </script>
 
 <template>
+  <main>
+    <VTaskBoard title="To Do"/>
+    <VTaskBoard title="In Progress"/>
+    <VTaskBoard title="Done"/>
+  </main>
 </template>
 
 <style scoped>
+  main {
+    display: flex;
+    justify-content: space-between;
+  }
 </style>
