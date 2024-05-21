6f8022ea3b0a58831fc05d1121cb541c0fb78996
Tristan Jay J. Virtudazo <tj.virtudazo@gmail.com>
2024-05-21 13:56:41 +0800

    modify: add edit emits in task item to emit back the target task object

--- src/components/VTaskItem.vue
+++ src/components/VTaskItem.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 	const props = defineProps<{ task?: Task }>()
-	const emits = defineEmits(['start', 'complete'])
+	const emits = defineEmits(['start', 'complete', 'edit'])
 </script>
 
 <template>
@@ -11,7 +11,7 @@
 			<p>{{ task.description ? task.description : "None" }}</p>
 		</div>
 		<div class="btn__group">
-			<button>Edit</button>
+			<button @click="emits('edit', task)">Edit</button>
 			<button @click="emits('start', task.id)">Start task</button>
 			<button @click="emits('complete', task.id)">Mark as complete</button>
 		</div>
